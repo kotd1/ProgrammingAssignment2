c4b90e2521d0bee80d4c9b409f4fb23a52fc338b
kot <dankot@gmail.com>
2014-08-21 11:00:51 +0300

    Commiting my version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+### These two functions take an invertible matrix, make it cacheable
+### inverse the matrix, and return it inversed, either from cache or directly from computation.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The makeCacheMatrix() function takes a matrix and creates a special object - a "matrix" that can cache its inverse.
 
+makeCacheMatrix <- function(x = matrix()) {   # the function's input is an invertable matrix
+  s <- NULL                             # s for "solved", resetting the inverted matrix value
+  set <- function(y) {                  # a function that would allow to recreate the invertable matrix, with a new value (y)
+    x <<- y                             # superassigning a new value to x
+    s <<- NULL                          # resetting the cached inverted matrix, using superassignment
+  }                     
+                                        # The following funcions would be called by the cacheSolve function,
+                                        # or the user:
+  get <- function() {x}                 # get - this function returns the original, non-inverted matrix
+  setinv <- function(solve) s <<- solve # setinv - this function will cache the inverted matrix 
+                                        #   in the first run of cacheSolve, using a superassignment
+  getinv <- function() {s}              # getinv - a function that returns the cached inverted matrix
+  list(set = set, get = get,            # a list of the created functions that makes them accessiable
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## this function uses the makeCacheMatrix to invert the matrix, cache it and return it. 
+## its input is the output of makeCacheMatrix()
+  
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {         # its input in makeCacheMatrix's output
+                                         
+  s <- x$getinv()                       # calling the getinv function to give s the value of the cached matrix
+  if(!is.null(s)) {                     # checking if the matrix was already cached.  if it was:
+    message("getting cached data")      # printing this message
+    return(s)                           # returning the cached matrix 
+  }
+  data <- x$get()                       # if there was no cached inverted matrix - 
+  s <- solve(data, ...)                 # the matrix gets caculated
+  x$setinv(s)                           # cached
+  s                                     # returning the inverted matrix
 }
